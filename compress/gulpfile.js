d2743e78d8dc0d8c3153f1fc4841b02c1c24aec7
caocaoer <caocaoer1019@163.com>
2016-08-17 10:18:36 +0800

    update

--- compress/gulpfile.js
+++ compress/gulpfile.js
@@ -1,36 +1,49 @@
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),//js压缩
-    cleancss = require('gulp-clean-css')/*,
-    cssverison = require('gulp-make-css-url-version')*/;
+    htmlMin = require('gulp-htmlmin'), //html压缩
+    jsMin = require('gulp-uglify'), //js压缩
+    cssMin = require('gulp-clean-css'), //css压缩
+    notify = require('gulp-notify'), //提示信息
+    concat = require('gulp-concat'), //文件合并
+    rename = require('gulp-rename'); //文件重命名
 
 gulp.task('testCssMin', function (){
     gulp.src('src/css/*.css')
-        .pipe(cleancss({
+        .pipe(concat('main.css'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(cssMin({
             advanced: false, //类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
             compatibility: true, //保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
             keepBreaks: false, //类型：Boolean 默认：false [是否保留换行]
             keepSpecialComments: '*' //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
         }))
-//        .pipe(cssverison()) //给css文件里引用文件加版本号（文件MD5）
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('dist/css'))
+        .pipe(notify({
+            message: 'css task ok'
+        }));
 });
 
 gulp.task('testJsMin', function(){
     gulp.src('src/js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-});
-    
-/*gulp.task('cleancss', function(){
-    return gulp.src('css/*.css') //要压缩的css文件
-        .pipe(cleancss()) //执行压缩
-        .pipe(gulp.dest('dist/css')); //输出文件夹
-});*/
-
-/*gulp.task('clean', function (cb){
-    del(['dist'], cb);
+        .pipe(jsMin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+//        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(notify({
+            message: 'js task ok'
+        }));
 });
 
-gulp.task('default', ['clean'], function (){
-    gulp.start(['cleancss']);
-});*/
+gulp.task('testHtmlMin', function(){
+    gulp.src('src/*.html')
+    .pipe(htmlMin({
+        collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('dist'))
+    .pipe(notify({
+        message: 'html task ok'
+    }));
+});
